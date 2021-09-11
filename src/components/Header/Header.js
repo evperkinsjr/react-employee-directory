a0a267dc64864fd556236e301cd850b0316ff1b2
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-11 19:02:54 -0400

    added title and brief description; exported Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Employee Directory</h1>
+            <p>Names, photos, and contact information</p>
+        </header>
+    );
+};
+
+export default Header;
