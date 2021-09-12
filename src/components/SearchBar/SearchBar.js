3e15ae27bbaad0c39cae04f2730c37bc1a20b15f
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-12 11:59:47 -0400

    centered the search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const SearchBar = (props) => {
     return (
-        <div className="justify-content-center">
-            <form onClick={props.handleFormSubmit}>
+        <div>
+            <form onClick={props.handleFormSubmit} className="w-75 mx-auto">
                 <input
                     onChange={props.handleInputChange}
                     value={props.value}
                     name="search"
                     type="text"
-                    className="form-control"
+                    className="form-control text-center"
                     placeholder="Search by Name"
                 />
             </form>
